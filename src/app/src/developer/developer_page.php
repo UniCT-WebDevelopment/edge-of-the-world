c26bd642f29a6288a671f9db215c11f308b475bb
voidmainvoid95 <alexpitrolo95@gmail.com>
2017-07-04 16:18:16 +0200

    Aggiunte le cartelle e le pagine di prova per cliente e sviluppatore

---
+++ src/app/src/developer/developer_page.php
@@ -0,0 +1,17 @@
+<?php
+?>
+
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<p>Developer Page</p>
+</body>
+</html>
+
